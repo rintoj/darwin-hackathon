c6ffed7469ab9981344dd170955c641e5a4bc0a3
Aswin S <860418awns@01HW801739.local>
2016-08-12 14:42:50 +0530

    Adding line chart

--- app/app/pages/about/about.ts
+++ app/app/pages/about/about.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import {LineChart} from '../../components/line-chart';
+
 @Component({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/about/about.html',
+    directives: [LineChart]
 })
 export class AboutPage {
+
+  private data:number[];
   constructor(private navCtrl: NavController) {
+    this.data=[1000,70];
   }
 }
